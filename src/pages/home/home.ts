8f88af0000ee9d2f66018a90364a52cdfe1b0e46
Igo Jeferson <igojeferson.coelho@gmail.com>
2018-08-31 15:11:13 -0300

    - Inclusão de loading e alert na página de listagem de carros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, LoadingController, NavController } from 'ionic-angular';
 import { Carro } from "../../modulos/carro";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 
 @Component( {
   selector: 'page-home',
@@ -11,12 +11,34 @@ export class HomePage {
 
   public carros: Carro[];
 
-  constructor( public navCtrl: NavController, private _http: HttpClient ) {
+  constructor( public navCtrl: NavController,
+               private _http: HttpClient,
+               private _loadingCtrl: LoadingController,
+               private _alertCtrl: AlertController ) {
 
-    this._http.get<Carro[]>( "http://localhost:8080/api/carro/listaTodos" )
+    let loading = this._loadingCtrl.create( {
+      content: 'Carregando carros... '
+    } );
+
+    loading.present();
+
+    this._http.get<Carro[]>( "http://localhost:8080/api/carro/listaTodosz" )
       .subscribe(
         ( carros ) => {
-          this.carros = carros
+          this.carros = carros;
+          loading.dismiss();
+        },
+        ( err: HttpErrorResponse ) => {
+
+          loading.dismiss();
+
+          this._alertCtrl.create({
+            title: 'Falha na conexão',
+            subTitle: 'Não foi possivel carregar a lista de carros. Tente novamente mais tarde.',
+            buttons: [
+              { text: 'Ok' }
+              ]
+          }).present();
         }
       );
 
